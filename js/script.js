3d4f7ca0f48bf9ae792c8c1dedb761a2724bfbfa
iam-developer <turgunbaaev@gmail.com>
2020-09-02 02:08:51 +0300

    Added slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     $(window).on('scroll', function() {
-      console.log('Scrolled');
+      if($(this).scrollTop() != 0) {
+        $('header').css('display', 'none');
+        $('.bird').css('display', 'block');
+      }
+      else {
+        $('header').css('display', 'flex');
+        $('.bird').css('display', 'none');
+      }
+
         if($(window).scrollTop()) {
             $('header').addClass('fixed-menu');
             $('.menu-items a').addClass('color');
@@ -16,6 +24,33 @@ $(document).ready(function() {
             $('header').css('padding', '10px 40px');
         }
     });
+    $( ".bird" ).mouseover(function() {
+      $(this).css('display', 'none');
+      $('header').css('display', 'flex');
+    });
+
+
+    $('.heroes-img').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      autoplay:true,
+      autoplaySpeed:2000,
+      pauseOnFocus: false,
+      waitForAnimate: false,
+      slidesToScroll: 1, 
+    });
   });
 
-    
+// Change super heroes
+
+// let heroe = document.querySelectorAll('.hero');
+
+// function superHeroes() {
+//   for(let i = 0; i < heroe.length; i ++) {
+//     heroe[i].style.display = 'none';
+//     heroe[5].style.display = 'block';
+//   }
+// }
+
+// superHeroes();
